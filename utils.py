b92db6947be24b02b6baa5c5ac63ca27ea99a9d1
aprilcoffee <apricloffee@github.com>
2023-01-23 19:33:53 +0100

    add api

--- utils.py
+++ utils.py
@@ -10,6 +10,14 @@ import time
 from diffusers import StableDiffusionImg2ImgPipeline
 from io import BytesIO
 
+import config
+
+from base64 import b64decode
+import openai
+import stability
+from stability_sdk import client
+import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
+
 
 def dummy(images, **kwargs):
 		return images, False   
@@ -22,10 +30,13 @@ class sd:
         self.DIR.mkdir(parents=True,exist_ok=True)
  
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        if mode == 'upscale':
+        if mode == 'upscale_realesrgan':
             self.model = RealESRGAN(self.device, scale=4)
             self.model.load_weights('weights/RealESRGAN_x4.pth', download=True)
-        elif mode == 'img2img':
+        elif mode == 'upscale_stablediffusion':
+            self.model = RealESRGAN(self.device, scale=4)
+            self.model.load_weights('weights/RealESRGAN_x4.pth', download=True)
+        elif mode == 'img2img_stablediffusion':
             self.model_id_or_path = "runwayml/stable-diffusion-v1-5"
             self.pipe = StableDiffusionImg2ImgPipeline.from_pretrained(
                 self.model_id_or_path,
@@ -35,11 +46,15 @@ class sd:
             self.pipe = self.pipe.to(self.device)
             self.pipe.safety_checker = dummy
     
-    def upscale_RealESRGAN(self,img):
+    def upscale_realesrgan(self,img):
         image = self.model.predict(img)
         return image
 
-    def img2img(self,img,prompt=''):
+    def upscale_stablediffusion(self,img):
+        image = self.model.predict(img)
+        return image
+
+    def img2img_stablediffusion(self,img,prompt='', negative_prompt='', strength=0.5, guidance_scale=5, ):
         image = self.pipe(prompt=prompt, init_image=img, strength=0.55, guidance_scale=6).images[0]
         return image
    
