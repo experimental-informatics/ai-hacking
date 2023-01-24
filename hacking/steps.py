8c982f885113b7534c8f030e84239ae3714a050a
aprilcoffee <apricloffee@github.com>
2023-01-24 18:21:29 +0100

    black white and green

--- hacking/steps.py
+++ hacking/steps.py
@@ -7,31 +7,34 @@ import time
 import os
 
 
-stepsCounter = [32,64,128,256,512]
+stepsCounter = [16,32,64,128,256,512]
 
 for k in stepsCounter:
-    m = AI('img2img_stablediffusion',dirName=("steps/" + str(k)))
+    m = AI('img2img_stablediffusion',dirName=("steps_BW/" + str(k)))
 
     width = 512
     height = 512
     url = f"https://picsum.photos/{width}/{height}"
-    image = Image.open(requests.get(url, stream=True).raw).convert("RGB")
+    image = Image.open(requests.get(url, stream=True).raw).convert("L").convert('RGB')
     image = image.resize((width, height))
 
     i = 0
     filename = str(i).zfill(5) + '.jpg'
     image.save(m.dir / filename)
 
-    while i < 70:
+    while i < 150:
         i = i + 1
         #image = image.resize((width / 4, height / 4))
         image = m.run(img=image,num_inference_steps=k)
+        image = image.convert("L").convert('RGB')
         filename = str(i).zfill(5) + '.jpg'
         image.save(m.dir / filename)
            
 
+
+
 for k in stepsCounter:
-    m = AI('img2img_stablediffusion',dirName=("steps_apple/" + str(k)))
+    m = AI('img2img_stablediffusion',dirName=("steps_green/" + str(k)))
 
     width = 512
     height = 512
@@ -43,10 +46,10 @@ for k in stepsCounter:
     filename = str(i).zfill(5) + '.jpg'
     image.save(m.dir / filename)
 
-    while i < 70:
+    while i < 150:
         i = i + 1
         #image = image.resize((width / 4, height / 4))
-        image = m.run(prompt="apple",img=image,num_inference_steps=k)
+        image = m.run(prompt="green",img=image,num_inference_steps=k)
         filename = str(i).zfill(5) + '.jpg'
         image.save(m.dir / filename)
            
